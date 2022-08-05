690eee71e9aeac5bb5ecd7f4b17558f28051b40b
Stephen <stephen.lyssy@gmail.com>
2022-08-05 06:22:27 -0500

    Wrote function to remove special characters and count the number of time each letter appears in a string.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict';
 const prompt = require('prompt-sync')({ sigint: true });
-const number = prompt(
-  'Enter a positive number to determine how many ramp numbers exist between 1 and that number. Enter number:'
-);
+// const number = prompt(
+//   'Enter a positive number to determine how many ramp numbers exist between 1
+//   and that number. Enter number: '
+// );
 
 // $ Function to convert a number to an array.
 const numToArray = (num) => Array.from(String(num), Number);
@@ -58,8 +59,39 @@ const countRampNumbers = (num) => {
   );
 };
 
-countRampNumbers(number);
+// countRampNumbers(number);
 
+const string = prompt('Enter a word, paragraph or novel: ');
+
+// $ Create an array of acceptable characters.
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
-const alphabet = alpha.map((x) => String.fromCharCode(x));
-console.log(alphabet);
+const alphabetLower = alpha.map((x) => String.fromCharCode(x).toLowerCase());
+const alphabetCaps = alpha.map((x) => String.fromCharCode(x));
+const alphabet = alphabetCaps.concat(alphabetLower);
+
+//$ Removing special characters and numbers from the string.
+const charsOnly = string.replace(/[^a-zA-Z]/g, '');
+
+// $Adding characters to a string
+const arrayOfChars = [...charsOnly];
+
+// $ Loop over the array of characters and count each character and counting
+// $each time a character appears and putting results in a count object.
+const count = {};
+
+for (const element of arrayOfChars) {
+  if (count[element]) {
+    count[element] += 1;
+  } else {
+    count[element] = 1;
+  }
+}
+
+// $ Create an array of the keys from the count object.
+const keys = Object.keys(count);
+
+// $ Loop over the keys array and using each key to grab the value for each key
+// $in the count object.
+keys.forEach((key) => {
+  console.log(`${key}: ${count[key]}`);
+});
