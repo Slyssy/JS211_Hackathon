3171204d24219cc7e09e245467f08712ba31dbc6
Stephen <stephen.lyssy@gmail.com>
2022-08-05 05:17:42 -0500

    Getting ready to start Prompt 3

--- main.js
+++ main.js
@@ -59,3 +59,7 @@ const countRampNumbers = (num) => {
 };
 
 countRampNumbers(number);
+
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabet = alpha.map((x) => String.fromCharCode(x));
+console.log(alphabet);
